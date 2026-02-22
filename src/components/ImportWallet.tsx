5d5432ace4b5dcaf98be37a2b4ff9aeae7d9f147
jimjoice2804 <jimjoice28@gmail.com>
2026-02-22 12:21:21 +0530

    add routes and its corresponding components

---
+++ src/components/ImportWallet.tsx
@@ -0,0 +1,5 @@
+const ImportWallet = () => {
+  return <div>ImportWallet</div>;
+};
+
+export default ImportWallet;
---
+++ src/components/Recieve.tsx
@@ -0,0 +1,5 @@
+const Recieve = () => {
+  return <div>Recieve</div>;
+};
+
+export default Recieve;
